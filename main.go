a5794427bf29576a211ef6a2633042279be3ac86
Reggie Riser <riser@rmfam.com>
2019-10-13 10:15:55 -0400

    Add Allow header to note allowed request methods

--- main.go
+++ main.go
@@ -23,13 +23,11 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	// Use r.Method to check whether the request is using POST or not. Note that
-	// http.MethodPost is a constant equal to the string "POST".
 	if r.Method != http.MethodPost {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", http.MethodPost)
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
 		return
